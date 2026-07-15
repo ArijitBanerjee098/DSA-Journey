178f2bcb7602943ee26ad303e1328620ef4ac49e
ArijitBanerjee098 <banerjeeari70@gmail.com>
2026-07-15 12:27:35 +0530

    .

---
+++ Star_print/ptr11.py
@@ -0,0 +1,8 @@
+#A print
+for row in range(7):
+    for col in range(5):
+        if (col==0 or col==4) or (row==0 or row==3) and (col>0 and col<4):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
