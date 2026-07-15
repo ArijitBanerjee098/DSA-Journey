a13ee5de78c223e5ff987290be8e925710eb1a62
ArijitBanerjee098 <banerjeeari70@gmail.com>
2026-07-15 18:33:37 +0530

    .

---
+++ Star_print/ptr14.py
@@ -0,0 +1,9 @@
+
+n=int(input("Enter the num: "))
+for i in range(n):
+    for j in range(n):
+        if i+j==2 or j-i==2 or i-j==2 or i+j==6:
+            print("*", end="")
+        else: 
+            print(end=" ")
+    print()
