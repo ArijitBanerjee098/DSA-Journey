9439b0534dfeddea897072f8d86c218cf42e4cb9
ArijitBanerjee098 <banerjeeari70@gmail.com>
2026-07-16 19:28:30 +0530

    Butterfly pattern

---
+++ Star_print/ptr17.py
@@ -0,0 +1,18 @@
+for i in range(1,6):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    for s in range(1, (5-i)*2+1):
+        print(" ", end=" ")
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+for i in range(4,0,-1):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    for s in range(1, (5-i)*2+1):
+        print(" ", end=" ")
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+
+
